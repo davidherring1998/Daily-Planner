b091630b70b81dd7ba9547d6fc44993c152bb121
David Herring <davidherring@Davids-MBP.home>
2022-08-31 11:49:53 -0500

    adding time stamps and event blocks

---
+++ Develop/apps.js
@@ -0,0 +1,119 @@
+
+//setting current day and time 
+const currentDay = document.getElementById('currentDay');
+const current = moment().format('llll');
+currentDay.textContent = current;
+
+
+// changing color based on time 
+const event9 = document.getElementsByClassName('event-block-9');
+const event10 = document.getElementsByClassName('event-block-10');
+const event11 = document.getElementsByClassName('event-block-11');
+const event12 = document.getElementsByClassName('event-block-12');
+const event1 = document.getElementsByClassName('event-block-1');
+const event2 = document.getElementsByClassName('event-block-2');
+const event3 = document.getElementsByClassName('event-block-3');
+const event4 = document.getElementsByClassName('event-block-4');
+const event5 = document.getElementsByClassName('event-block-5');
+
+const myTime = new Date ();
+// hour 9
+if(myTime.getHours() > 8 && myTime.getHours() < 9 )  {
+    $(event9).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() > 9 ) {
+    console.log('past 9pm')
+    $(event9).css('background-color', 'lightblue');
+} else {
+    $(event9).css('background-color', 'green');
+}
+
+//hour 10
+if(myTime.getHours() >= 10 && myTime.getHours() < 11 )  {
+    $(event10).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 11 ) {
+    console.log('past 10pm')
+    $(event10).css('background-color', 'lightblue');
+} else {
+    $(event10).css('background-color', 'green');
+}
+
+//hour 11 
+if(myTime.getHours() >= 11 && myTime.getHours() < 12 )  {
+    $(event11).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 12 ) {
+    console.log('past 11pm')
+    $(event11).css('background-color', 'lightblue');
+} else {
+    $(event11).css('background-color', 'green');
+}
+
+//hour 12
+if(myTime.getHours() >= 12 && myTime.getHours() < 13 )  {
+    $(event12).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 12 ) {
+    console.log('past 12pm')
+    $(event12).css('background-color', 'lightblue');
+} else {
+    $(event12).css('background-color', 'green');
+}
+
+// 1pm
+if(myTime.getHours() >= 13 && myTime.getHours() < 14 )  {
+    $(event1).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 13 ) {
+    console.log('past 1pm')
+    $(event1).css('background-color', 'lightblue');
+} else {
+    $(event1).css('background-color', 'green');
+}
+
+// 2pm
+if(myTime.getHours() >= 14 && myTime.getHours() < 15 )  {
+    $(event2).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 15 ) {
+    console.log('past 2pm')
+    $(event2).css('background-color', 'lightblue');
+} else {
+    $(event2).css('background-color', 'green');
+}
+
+// 3pm
+if(myTime.getHours() >= 15 && myTime.getHours() < 16 )  {
+    $(event3).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 16 ) {
+    console.log('past 3pm')
+    $(event3).css('background-color', 'lightblue');
+} else {
+    $(event3).css('background-color', 'green');
+}
+
+// 4pm
+if(myTime.getHours() >= 16 && myTime.getHours() < 17 )  {
+    $(event4).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 17 ) {
+    console.log('past 4pm')
+    $(event4).css('background-color', 'lightblue');
+} else {
+    $(event4).css('background-color', 'green');
+}
+
+// 5pm
+if(myTime.getHours() >= 17 && myTime.getHours() < 18 )  {
+    $(event5).css('background-color', 'red');
+    console.log('current');
+} else if (myTime.getHours() >= 18 ) {
+    console.log('past 1pm')
+    $(event5).css('background-color', 'lightblue');
+} else {
+    $(event5).css('background-color', 'green');
+}
+
+//
